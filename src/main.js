23fdf9f3b1c785dab98d3dec2849f443ee327d9d
xiongzk <775126135@qq.com>
2020-02-13 11:54:39 +0800

    第36课作业

--- src/main.js
+++ src/main.js
@@ -56,7 +56,7 @@
 
 // console.log(dom.index(s2));
 
-const div = dom.find("#test>.red")[0]; // 获取对应的元素
+const div = dom.find("#test>.red")[0]; // 获取对应元素
 dom.style(div, "color", "red"); // 设置 div.style.color
 
 const divList = dom.find(".red"); // 获取多个 div.red 元素

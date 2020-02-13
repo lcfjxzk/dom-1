756b850c1bcc5a8a5397978ec348932d36f1ea24
xiongzk <775126135@qq.com>
2020-02-13 12:45:25 +0800

    第36课作业

--- src/main.js
+++ src/main.js
@@ -1,3 +1,9 @@
+const div = dom.find("#test>.red")[0]; // 获取对应元素
+dom.style(div, "color", "red"); // 设置 div.style.color
+
+const divList = dom.find(".red"); // 获取多个 div.red 元素
+dom.each(divList, n => console.log(n)); // 遍历 divList 里的所有元素
+
 // const div = dom.create("<div>newDiv</div>");
 // console.log(div);
 
@@ -55,9 +61,3 @@
 // dom.each(dom.children(t), n => dom.style(n, "color", "red"));
 
 // console.log(dom.index(s2));
-
-const div = dom.find("#test>.red")[0]; // 获取对应元素
-dom.style(div, "color", "red"); // 设置 div.style.color
-
-const divList = dom.find(".red"); // 获取多个 div.red 元素
-dom.each(divList, n => console.log(n)); // 遍历 divList 里的所有元素
